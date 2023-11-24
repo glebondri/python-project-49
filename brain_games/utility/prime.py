b8f751931146749124297e7af32725e9286cb618
sheref <launcher.zet@gmail.com>
2023-11-24 17:44:32 +0300

    modified utility/prime.py

--- brain_games/utility/prime.py
+++ brain_games/utility/prime.py
@@ -1,42 +1,36 @@
 def is_prime_number(number):
-    if number > 2:
-        last_index = number + 1
-        prime_numbers = [i + 1 for i in range(1, last_index)]
 
+    last_index = number + 1
+    prime_numbers = [i + 1 for i in range(1, last_index)]
+
+    '''
+     2  3  4  5  6  7  8  9  10
+    11 12 13 14 15 16 17 18 19 20
+          ->     ...
+    '''
+    for index in range(2, last_index):
         '''
-         2  3  4  5  6  7  8  9  10
-        11 12 13 14 15 16 17 18 19 20
+         >>2<<  3  4  5  6  7  8  9  10
+          11   12 13 14 15 16 17 18 19 20
               ->     ...
         '''
-        for index in range(2, last_index):
-            '''
-             >>2<<  3  4  5  6  7  8  9  10
-              11   12 13 14 15 16 17 18 19 20
-                  ->     ...
-            '''
-            if index in prime_numbers:
-                for n in prime_numbers:
-                    '''
-                    "is number in list is multiple to given number?"
+        if index in prime_numbers:
+            for n in prime_numbers:
+                '''
+                "is number in list is multiple to given number?"
 
-                    >>2<<  3  [4]  5  [6]  7  [8]  9  [10]
-                     11  [12] 13 [14] 15 [16] 17 [18] 19 [20]
-                        ->     ...
-                    '''
+                >>2<<  3  [4]  5  [6]  7  [8]  9  [10]
+                 11  [12] 13 [14] 15 [16] 17 [18] 19 [20]
+                    ->     ...
+                '''
 
-                    if n != index and n % index == 0:
-                        '''removing all the 'multiple' numbers'''
-                        prime_numbers.remove(n)
+                if n != index and n % index == 0:
+                    '''removing all the 'multiple' numbers'''
+                    prime_numbers.remove(n)
 
-        return number in prime_numbers
-
-    elif number == 2:
-        return True
-
-    else:
-        return False
+    return number in prime_numbers
 
 
 if __name__ == '__main__':
-    print(is_prime_number(7))
+    print(is_prime_number(8))
     print(is_prime_number(12))
