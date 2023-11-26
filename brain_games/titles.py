df32dc19faf5f2385931e01042a4e1eca2974fc8
launc <toshadrazdov@gmail.com>
2023-11-26 17:29:50 +0300

    modified brain_games/titles.py

--- brain_games/titles.py
+++ brain_games/titles.py
@@ -1,5 +1,5 @@
 QUESTION = 'Question: {}\nYour answer: '
-WRONG_ANSWER = "'{}' is wrong answer ;(. Correct answer was '{}'.\n"
+WRONG_ANSWER = "'{}' is wrong answer ;(. Correct answer was '{}'."
 TRY_AGAIN = "Let's try again, {}!"
 
 
