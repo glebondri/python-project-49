9310670921b0f53f5a6eac9b336d962dbeedf6c1
launc <toshadrazdov@gmail.com>
2023-11-28 17:57:24 +0300

    modified brain_games/utility/randomizer.py

--- brain_games/utility/randomizer.py
+++ brain_games/utility/randomizer.py
@@ -3,12 +3,16 @@ from random import randint
 memory = []
 
 
-def unique(first, last):  # used to generate unique random number
+def unique(first, last):  # used to generate unique random numbers
     global memory
 
     random_number = randint(first, last)
     if random_number in memory:
-        return unique(first, last)
+        try:
+            return unique(first, last)
+        except RecursionError:
+            print("Seems, I can't offer you more numbers than you asked for!")
+            exit(0)
 
     memory += [random_number]
     return random_number
