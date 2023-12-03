40aaf918717088f75c65191e836b7383fcbdc0eb
launc <toshadrazdov@gmail.com>
2023-12-03 18:30:49 +0300

    add brain_games/games/progression.py

---
+++ brain_games/games/progression.py
@@ -0,0 +1,21 @@
+from random import randint, choice
+
+description = 'What number is missing in the progression?'
+
+
+def new():
+    random_number = randint(1, 15)
+    step = randint(2, 5)
+
+    sequence = []
+    sequence_length = 10
+    for index in range(sequence_length):
+        sequence += [random_number + step * index]
+
+    missing_number = choice(sequence)
+
+    question = ' '.join(
+        (str(n) if n != missing_number else '..') for n in sequence)
+    answer = missing_number
+
+    return question, answer
