eacca6ca3ca1e4eb0b6ebe3be6121adce0b33172
launc <toshadrazdov@gmail.com>
2023-12-03 18:32:51 +0300

    modified brain_games/titles.py

--- brain_games/titles.py
+++ brain_games/titles.py
@@ -1,10 +1,3 @@
 QUESTION = 'Question: {}\nYour answer: '
 WRONG_ANSWER = "'{}' is wrong answer ;(. Correct answer was '{}'."
 TRY_AGAIN = "Let's try again, {}!"
-
-
-CALC = 'What is the result of the expression?'
-EVEN = 'Answer "yes" if the number is even, otherwise answer "no".'
-GCD = 'Find the greatest common divisor of given numbers.'
-PROGRESSION = 'What number is missing in the progression?'
-PRIME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
