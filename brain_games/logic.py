b76b758c5e62cde07a7e5ab3da2173b150191fc5
launc <toshadrazdov@gmail.com>
2023-12-03 18:32:19 +0300

    modified brain_games/logic.py

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -2,28 +2,51 @@ from brain_games import user
 from brain_games.titles import QUESTION, WRONG_ANSWER, TRY_AGAIN
 
 
-def ask_questions(title, qna):
-
-    def unpack(dictionary, index):
-        return index, dictionary[index]
+def ask_to_answer(qna):
+    print('Welcome to the Brain Games!')
 
     username = user.ask_for_name()
+    print(f'Hello, {username}!')
 
-    if len(qna) > 1:  # is there is anything to ask?
-        print(title)
+    print(qna.description)
 
-        for q in qna:
-            question, answer = unpack(qna, q)
-            user_answer = input(QUESTION.format(str(question)))
+    for attempt in range(3):
+        question, answer = qna.new()
 
-            if str(user_answer) != str(answer):
-                print(WRONG_ANSWER.format(user_answer, answer))
-                print(TRY_AGAIN.format(username))
-                exit(0)
+        user_answer = input(QUESTION.format(str(question)))
 
-            print('Correct!')
+        if str(user_answer) != str(answer):
+            print(WRONG_ANSWER.format(user_answer, answer))
+            print(TRY_AGAIN.format(username))
+            exit(0)
 
-        print(f'Congratulations, {username}!')
+        print('Correct!')
 
-    else:
-        print("I don't know what to ask you!")
+    print(f'Congratulations, {username}!')
+
+
+# def ask_questions(title, qna):
+#
+#     def unpack(dictionary, index):
+#         return index, dictionary[index]
+#
+#     username = user.ask_for_name()
+#
+#     if len(qna) > 1:  # is there is anything to ask?
+#         print(title)
+#
+#         for q in qna:
+#             question, answer = unpack(qna, q)
+#             user_answer = input(QUESTION.format(str(question)))
+#
+#             if str(user_answer) != str(answer):
+#                 print(WRONG_ANSWER.format(user_answer, answer))
+#                 print(TRY_AGAIN.format(username))
+#                 exit(0)
+#
+#             print('Correct!')
+#
+#         print(f'Congratulations, {username}!')
+#
+#     else:
+#         print("I don't know what to ask you!")
