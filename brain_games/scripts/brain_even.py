85f380e606486aee2ecb5c8725d18d2f6212aae8
launc <toshadrazdov@gmail.com>
2023-12-04 14:15:02 +0300

    modified brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,17 +1,6 @@
 from brain_games import logic
-from brain_games import titles
-from brain_games.utility import randomizer
+from brain_games.games import even
 
 
 def main():
-
-    qna_pair = {}  # dict of 'questions 'n' answers'
-
-    for i in range(3):
-        question = randomizer.unique(1, 6)
-        answer = 'yes' if question % 2 == 0 else 'no'
-
-        qna_pair[question] = answer
-
-    logic.ask_questions(qna=qna_pair,
-                        title=titles.EVEN)
+    logic.ask_to_answer(even)
