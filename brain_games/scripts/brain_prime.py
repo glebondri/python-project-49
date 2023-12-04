abefff79db477094b0fc3e9ca684e4a19f43b69a
launc <toshadrazdov@gmail.com>
2023-12-04 14:17:23 +0300

    modified brain_games/scripts/brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,17 +1,6 @@
 from brain_games import logic
-from brain_games import titles
-from brain_games.utility import randomizer, prime
+from brain_games.games import prime
 
 
 def main():
-
-    qna_pair = {}  # dict of 'questions 'n' answers'
-
-    for i in range(3):
-        question = randomizer.unique(1, 40)
-        answer = 'yes' if prime.is_prime_number(question) else 'no'
-
-        qna_pair[question] = answer
-
-    logic.ask_questions(qna=qna_pair,
-                        title=titles.PRIME)
+    logic.ask_to_answer(prime)
