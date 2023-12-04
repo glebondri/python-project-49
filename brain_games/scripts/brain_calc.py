be1ea18300c040a7c2cb768a3c9bb133c38e24f9
launc <toshadrazdov@gmail.com>
2023-12-04 14:15:56 +0300

    modified brain_games/scripts/brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,22 +1,6 @@
 from brain_games import logic
-from brain_games import titles
-from brain_games.utility import randomizer
-from random import choice
+from brain_games.games import calc
 
 
 def main():
-
-    qna_pair = {}  # dict of 'questions 'n' answers'
-
-    for i in range(3):
-        first_number = randomizer.unique(1, 50)
-        second_number = randomizer.unique(1, 50)
-        operator = choice(['+', '-', '*'])
-
-        question = f'{str(first_number)} {operator} {str(second_number)}'
-        answer = eval(question)
-
-        qna_pair[question] = answer
-
-    logic.ask_questions(qna=qna_pair,
-                        title=titles.CALC)
+    logic.ask_to_answer(calc)
