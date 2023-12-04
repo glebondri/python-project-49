7547c2ee2a157f5683deb1429ed0af5b1f42bc7c
launc <toshadrazdov@gmail.com>
2023-12-04 14:16:25 +0300

    modified brain_games/scripts/brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,21 +1,6 @@
 from brain_games import logic
-from brain_games import titles
-from brain_games.utility import randomizer
-from math import gcd as greatest_common_divisor
+from brain_games.games import gcd
 
 
 def main():
-
-    qna_pair = {}  # dict of 'questions 'n' answers'
-
-    for i in range(3):
-        first_number = randomizer.unique(1, 50)
-        second_number = randomizer.unique(1, 50)
-
-        question = f'{str(first_number)} {str(second_number)}'
-        answer = greatest_common_divisor(first_number, second_number)
-
-        qna_pair[question] = answer
-
-    logic.ask_questions(qna=qna_pair,
-                        title=titles.GCD)
+    logic.ask_to_answer(gcd)
