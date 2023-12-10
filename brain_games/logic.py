d9d4e01e9cbaa067a568301fd3ef49e99acf6ff1
launc <toshadrazdov@gmail.com>
2023-12-10 15:33:28 +0300

    modified brain_games/logic.py

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -23,30 +23,3 @@ def ask_to_answer(game):
         print('Correct!')
 
     print(f'Congratulations, {username}!')
-
-
-# def ask_questions(title, qna):
-#
-#     def unpack(dictionary, index):
-#         return index, dictionary[index]
-#
-#     username = user.ask_for_name()
-#
-#     if len(qna) > 1:  # is there is anything to ask?
-#         print(title)
-#
-#         for q in qna:
-#             question, answer = unpack(qna, q)
-#             user_answer = input(QUESTION.format(str(question)))
-#
-#             if str(user_answer) != str(answer):
-#                 print(WRONG_ANSWER.format(user_answer, answer))
-#                 print(TRY_AGAIN.format(username))
-#                 exit(0)
-#
-#             print('Correct!')
-#
-#         print(f'Congratulations, {username}!')
-#
-#     else:
-#         print("I don't know what to ask you!")
