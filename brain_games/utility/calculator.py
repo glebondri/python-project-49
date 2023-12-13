0a9061d36d9ad0130649ab1ca120af5f3210370e
launc <toshadrazdov@gmail.com>
2023-12-13 15:55:57 +0300

    modified brain_games/utility/calculator.py

--- brain_games/utility/calculator.py
+++ brain_games/utility/calculator.py
@@ -1,4 +1,13 @@
-def operate(a, b, operator):
+from typing import Optional
+
+
+def operate(a: int, b: int, operator: str) -> Optional[int]:
+    """Arithmetic operation over two numbers
+    :param a: First number
+    :param b: Second number
+    :param operator: Arithmetic operator (add, subtract or multiply)
+    :return: Result of arithmetic operation (None if operator is invalid)"""
+
     match operator:
         case '+':
             result = a + b
